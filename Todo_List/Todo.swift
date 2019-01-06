f4dd0a22671612577d9d2f3b044df203c5081bb2
Jayce Azua <jayce.azua@gmail.com>
2019-01-05 21:21:30 -0500

    todo app

---
+++ Todo_List/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  Todo_List
+//
+//  Created by Jayce Azua on 1/5/19.
+//  Copyright © 2019 Jayce Azua. All rights reserved.
+//
+
+import Foundation
+
+
+class Todo {
+    var title: String = ""
+    var important: Bool = true
+}
---
+++ Todo_List/TodoTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TodoTableViewController.swift
+//  Todo_List
+//
+//  Created by Jayce Azua on 1/5/19.
+//  Copyright © 2019 Jayce Azua. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    var todos = [Todo]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let todo1 = Todo()
+        todo1.title = "Get this fucking course done!"
+        todo1.important = true
+        
+        let todo2 = Todo()
+        todo2.title = "Finish more node courses"
+        todo2.important = true
+        
+        todos = [todo1, todo2]
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return todos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let currentTodo = todos[indexPath.row]
+        
+        if currentTodo.important {
+            cell.textLabel?.text = "⁉️\(currentTodo.title)"
+        }
+        else {
+            cell.textLabel?.text = currentTodo.title
+        }
+
+        return cell
+    }
+    
+}
